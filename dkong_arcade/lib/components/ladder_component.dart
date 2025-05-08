53bc5225d85448326e1f49047cc3c58aa7cc618d
Eduard <eduard.martini@onthegosystems.com>
2025-05-08 17:34:37 +0300

    updates to use images from assets

--- dkong_arcade/lib/components/ladder_component.dart
+++ dkong_arcade/lib/components/ladder_component.dart
@@ -1,18 +1,4 @@
 import "package:flame/components.dart";
-import "package:flame/flame.dart";
-import "package:flutter/material.dart" show Colors, Color, Canvas, Paint, Rect, PictureRecorder, ImageByteFormat;
-
-Future<Sprite> fallbackRectSprite(Vector2 size, Color color) async {
-  final recorder = PictureRecorder();
-  final canvas = Canvas(recorder);
-  final paint = Paint()..color = color;
-  canvas.drawRect(Rect.fromLTWH(0, 0, size.x, size.y), paint);
-  final pic = recorder.endRecording();
-  final img = await pic.toImage(size.x.toInt(), size.y.toInt());
-  final bytes = await img.toByteData(format: ImageByteFormat.png);
-  final image = await Flame.images.decodeImageFromList(bytes!.buffer.asUint8List());
-  return Sprite(image);
-}
 
 class LadderComponent extends SpriteComponent {
   LadderComponent({required Vector2 position, required Vector2 size})
@@ -20,11 +6,7 @@ class LadderComponent extends SpriteComponent {
 
   @override
   Future<void> onLoad() async {
-    try {
-      sprite = await Sprite.load("ladder.png");
-    } catch (_) {
-      sprite = await fallbackRectSprite(size, Colors.green);
-    }
+    sprite = await Sprite.load("ladder.png");
   }
 }
 
